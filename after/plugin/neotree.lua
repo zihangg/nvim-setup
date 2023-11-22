b6edb0a0c0ab9b779c6ef9965d3bc1f62b81c137
Zi Hang <kevu.lee@gmail.com>
2023-11-22 17:18:54 +0800

    fix: hide .git

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -21,6 +21,7 @@ require("neo-tree").setup({
             hide_gitignored = false,
             hide_hidden = false, -- only works on Windows for hidden files/directories
             hide_by_name = {
+              ".git"
                 -- "node_modules"
             },
             hide_by_pattern = { -- uses glob style patterns
